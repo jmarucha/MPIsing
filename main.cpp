0f6fe5143c615b61cd53e22f835df2f9d73190ec
Jan K. Marucha <jkmarucha@gmail.com>
2026-08-14 01:27:39 +0200

    minor tweaks

--- main.cpp
+++ main.cpp
@@ -23,7 +23,8 @@ enum opt_groups {
     OPT_GROUP_GENERAL,
     OPT_GROUP_GRID,
     OPT_GROUP_ROUNDS,
-    OPT_GROUP_PARAMS_PNG,
+    OPT_GROUP_PARAMS,
+    OPT_GROUP_PNG,
     OPT_GROUP_OTHER,
 };
 
@@ -31,41 +32,65 @@ enum opt_codes {
     OPT_SAVE_PNG,
     OPT_PNG_OUTPUT_DIR,
     OPT_PNG_EVERY_NTH_ROUND,
-    OPT_HELP
+    OPT_HELP,
+    OPT_J = 'j',
+    OPT_MU = 'm'
 };
 
 static struct argp_option options[] = {
   {"verbose", 'v', 0, 0, "Produce verbose output", OPT_GROUP_GENERAL},
-  {"width", 'w', "W", 0, "Grid width", OPT_GROUP_GRID},
-  {"height", 'h', "H", 0, "Grid height", OPT_GROUP_GRID},
-  {"flips", 'f', "FLIPS", 0, "Flips per round", OPT_GROUP_ROUNDS},
-  {"rounds", 'r', "ROUNDS", 0, "Total rounds", OPT_GROUP_ROUNDS},
-  {"save-png", OPT_SAVE_PNG, 0, 0, "Save png every few rounds", OPT_GROUP_PARAMS_PNG},
-  {"every-nth-round", OPT_SAVE_PNG, "N", 0, "Set PNG writing interval (in rounds)", OPT_GROUP_PARAMS_PNG},
-  {"output-dir", OPT_PNG_OUTPUT_DIR, "DIR", 0, "PNGs output directory", OPT_GROUP_PARAMS_PNG},
+  {"width", 'w', "INT", 0, "Grid width", OPT_GROUP_GRID},
+  {"height", 'h', "INT", 0, "Grid height", OPT_GROUP_GRID},
+  {"flips", 'f', "INT", 0, "Flips per round", OPT_GROUP_ROUNDS},
+  {"rounds", 'r', "INT", 0, "Total rounds", OPT_GROUP_ROUNDS},
+  {"save-png", OPT_SAVE_PNG, 0, 0, "Save png every few rounds", OPT_GROUP_PNG},
+  {"every-nth-round", OPT_PNG_EVERY_NTH_ROUND, "INT", 0, "Set PNG writing interval (in rounds)", OPT_GROUP_PNG},
+  {"output-dir", OPT_PNG_OUTPUT_DIR, "DIR", 0, "PNGs output directory", OPT_GROUP_PNG},
   {"help", OPT_HELP, 0, 0, "Show this help message", OPT_GROUP_OTHER},
   {"usage", OPT_HELP, 0, OPTION_ALIAS, 0, OPT_GROUP_OTHER},
+  {"j", OPT_J, "FLOAT", 0, "Spin-spin coupling j", OPT_GROUP_PARAMS},
+  {"mu", OPT_MU, "FLOAT", 0, "External magnetic field mu", OPT_GROUP_PARAMS},
   {0}
 };
 
 typedef struct arguments
 {
-    int w, h;
-    int rounds, flips;
+    long w, h;
+    double j, mu;
+    long rounds, flips;
     int verbose, save_png;
     char output_dir[256];
+    long every_nth_round;
 } arguments;
 
 
-error_t set_or_default(int num, const char* arg, int* target) {
-    if (num) {
-        *target = num;
-        return 0;
-    } else {
-        if (prank == 0) 
-            fprintf(stderr, "Expected number, got '%s'.\n", arg);
-        return ARGP_ERR_UNKNOWN;
+error_t try_seti(const char* arg, long int* target) {
+    if (arg) {
+        char* end_ptr = NULL;
+        long res = strtol(arg, &end_ptr, 10);
+        if (*end_ptr == '\0') {
+            *target = res;
+            return 0;
+        }
     }
+    if (prank == 0) 
+        fprintf(stderr, "Expected number, got '%s'.\n", arg);
+    return ARGP_ERR_UNKNOWN;
+}
+
+
+error_t try_setd(const char* arg, double* target) {
+    if (arg) {
+        char* end_ptr = NULL;
+        double res = strtod(arg, &end_ptr);
+        if (*end_ptr == '\0') {
+            *target = res;
+            return 0;
+        }
+    }
+    if (prank == 0) 
+        fprintf(stderr, "Expected number, got '%s'.\n", arg);
+    return ARGP_ERR_UNKNOWN;
 }
 
 enum parse_errors {
@@ -77,28 +102,31 @@ error_t
 parse_opt (int key, char *arg, struct argp_state *state)
 {
     arguments* args = (arguments*) state->input;
-    int num_arg = 0;
-    if (arg)
-        num_arg = atoi(arg);
   switch (key)
     {
     case 'v':
         args->verbose = 1;
         break;
     case 'w':
-        return set_or_default(num_arg, arg, &args->w);
+        return try_seti(arg, &args->w);
     case 'h':
-        return set_or_default(num_arg, arg, &args->h);
+        return try_seti(arg, &args->h);
     case 'f':
-        return set_or_default(num_arg, arg, &args->flips);
+        return try_seti(arg, &args->flips);
     case 'r':
-        return set_or_default(num_arg, arg, &args->rounds);
+        return try_seti(arg, &args->rounds);
+    case OPT_J:
+        return try_setd(arg, &args->j);
+    case OPT_MU:
+        return try_setd(arg, &args->mu);
     case OPT_SAVE_PNG:
         args->save_png = 1;
         break;
     case OPT_PNG_OUTPUT_DIR:
         strncpy(args->output_dir, arg, 256);
         break;
+    case OPT_PNG_EVERY_NTH_ROUND:
+        return try_seti(arg, &args->every_nth_round);
     case OPT_HELP:
         return HELP_MSG;
     default:
@@ -113,10 +141,12 @@ int main(int argc, char** argv) {
     error_t err;
     arguments args = {
         w: 1000, h: 1000,
+        j: 1.76, mu: 0.0,
         rounds: 10000, flips: 100,
+        output_dir: ".",
+        every_nth_round: 1,
     };
-    strcpy(args.output_dir, ".");
-    
+
     err = MPI_Init(&argc, &argv);
     if (err) {
         fputs("Cannot initialize MPI.\n", stderr);
@@ -143,24 +173,21 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Running process %d of %d\n", prank, psize);
     std::srand(std::time(0)+prank);
 
-    // for (float b = 0; b < 20.0; b+=0.05) {
-    float b = 0.5;
-        grid g(args.w, args.h, args.flips, 1.76, 0.);
-        for (int i = 0; i < args.rounds; ++i) {
-            g.round();
-            if (args.save_png) {
-                g.gather();
-                char filename[256+32];
-                sprintf(filename, "%s/ROUND=%d.png", args.output_dir, i);
-                g.save_png(filename);
+    grid g(args.w, args.h, args.flips, args.j, args.mu);
+    for (int i = 0; i < args.rounds; ++i) {
+        g.round();
+        if (args.save_png && (i % args.every_nth_round == 0)) {
+            g.gather();
+            char filename[256+32];
+            sprintf(filename, "%s/ROUND=%d.png", args.output_dir, i);
+            g.save_png(filename);
+        }
+    }
+    int mag = g.magnetization();
+    if (prank == 0) {
+        printf("%f, %d\n", args.mu, mag);
+    }
 
-            }
-        }
-        int mag = g.magnetization();
-        if (prank == 0) {
-            printf("%f, %d\n", b, mag);
-        }
-    // }
     MPI_Finalize();
     return 0;
 }
